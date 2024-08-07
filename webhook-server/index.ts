9ccc4e4526b4471b82711b43eaa11760632a6fad
Vivek Suthar <sutharvivek18@gmail.com>
2024-08-07 17:13:03 +0530

    back to what it is

--- webhook-server/index.ts
+++ webhook-server/index.ts
@@ -1,30 +1,27 @@
-import cluster from "cluster";
-import os from "os";
-import express from "express";
 import bodyParser from "body-parser";
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
+import { setupWatcher } from "./lib/priceWatch";
+import { addListener } from "process";
+import { WebhookEvent } from "./lib/types/EventData";
+import fetchTxnData from "./lib/api/socket.api";
+import executeTrade from "./lib/utils/excuteTrade";
+import {
+  buildIdFromEventData,
+  getHashMap,
+  initializeHashMap,
+} from "./lib/utils/chainTokensMap";
 import { registerLimitOrder } from "./lib/limit-order";
-import { initializeHashMap } from "./lib/utils/chainTokensMap";
 
 const PORT = 3000;
 
-if (cluster.isPrimary) {
-  const numCPUs = os.cpus().length;
-  console.log(`${numCPUs} Workers started`);
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
-  const app = express();
-  initializeHashMap();
-  app.use(bodyParser.json());
-  app.post("/webhook", (req: Request, res: Response) => {
-    registerLimitOrder(req, res);
-  });
-  app.listen(PORT, () => {
-    console.log(`Worker ${process.pid} started on port ${PORT}`);
-  });
-}
+const app = express();
+initializeHashMap();
+app.use(bodyParser.json());
+
+app.post("/webhook", (req: Request, res: Response) => {
+  registerLimitOrder(req, res);
+});
+
+app.listen(PORT, () => {
+  console.log(`WebHook server is running on port ${PORT}`);
+});
