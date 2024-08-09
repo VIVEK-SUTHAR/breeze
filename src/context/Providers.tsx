de7266c19e26f66bf34169cf2e642a61b43d3929
sahilkakwani9 <sahkakwani@gmail.com>
2024-08-09 18:41:05 +0530

    fix

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,15 +1,21 @@
 "use client";
-import '@rainbow-me/rainbowkit/styles.css';
-import { RainbowKitProvider, getDefaultConfig, lightTheme, midnightTheme } from '@rainbow-me/rainbowkit';
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  RainbowKitProvider,
+  getDefaultConfig,
+  lightTheme,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum, base } from "viem/chains";
-import { useTheme } from 'next-themes';
-import { APP_NAME } from '@/constants';
+import { useTheme } from "next-themes";
+import { APP_NAME } from "@/constants";
+import { useEffect } from "react";
 
 const config = getDefaultConfig({
   appName: APP_NAME,
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: "YOUR_PROJECT_ID",
   chains: [mainnet, polygon, optimism, arbitrum, base],
   ssr: true,
 });
@@ -18,17 +24,18 @@ const queryClient = new QueryClient();
 
 type ProvidersProps = {
   children: React.ReactNode;
-}
+};
 
 export default function Providers({ children }: ProvidersProps) {
-  const { theme } = useTheme()
-  const rainbowkitTheme = theme === "light" ? lightTheme() : midnightTheme()
+  const { theme, setTheme } = useTheme();
+  const rainbowkitTheme = theme === "light" ? lightTheme() : midnightTheme();
+  useEffect(() => {
+    setTheme("light");
+  }, []);
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
-          theme={rainbowkitTheme}
-        >
+        <RainbowKitProvider theme={rainbowkitTheme}>
           {children}
         </RainbowKitProvider>
       </QueryClientProvider>
