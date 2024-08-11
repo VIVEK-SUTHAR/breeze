55b644aa7bc72b32df1f6e807077f82fa5344e2c
sahilkakwani9 <sahkakwani@gmail.com>
2024-08-11 19:11:54 +0530

    fix: switch chains

--- src/components/actions/Limit.tsx
+++ src/components/actions/Limit.tsx
@@ -6,7 +6,12 @@ import chainData from "../../utils/Chains";
 import CustomDropdown from "../ui/CustomDropdown";
 import fetchTokensForChain from "@/utils/fetchTokens";
 import { TokenData } from "@/types";
-import { useAccount, useReadContract, useWriteContract } from "wagmi";
+import {
+  useAccount,
+  useReadContract,
+  useSwitchChain,
+  useWriteContract,
+} from "wagmi";
 import { abi } from "@/constants";
 import OrderHistory from "../ui/OrderHistory";
 import { parseUnits } from "viem";
@@ -46,7 +51,8 @@ function LimitOrder() {
   const [orderHistory, setOrderHistory] = useState<LimitOrder[]>([]);
 
   const { data: hash, writeContract } = useWriteContract();
-  const { address } = useAccount();
+  const { address, chainId } = useAccount();
+  const { switchChain } = useSwitchChain();
 
   useEffect(() => {
     setChains(chainData);
@@ -130,8 +136,12 @@ function LimitOrder() {
     );
   };
 
-  const placeLimitOrder = () => {
+  const placeLimitOrder = async () => {
     if (!fromChain || !toChain || !fromToken || !toToken) return;
+    if (chainId !== fromChain.chainId) {
+      console.log("chain is different");
+      switchChain({ chainId: fromChain.chainId });
+    }
     console.log(
       "got this data",
       address,
